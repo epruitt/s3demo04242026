898aa128e41a0a1e7c200799f323be43e304bde9
epruitt <epruitt223@gmail.com>
2026-04-24 08:57:00 -0500

    add dynamoDB table

--- lib/s3demo-stack.ts
+++ lib/s3demo-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib/core";
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 export class S3DemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -19,6 +20,14 @@ export class S3DemoStack extends cdk.Stack {
       bucketName: "demos3bucket223",
       versioned: true,
       publicReadAccess: false,
+    });
+
+    // dynamoDB table
+    const dynamoDBdemo = new dynamodb.Table(this, "dynamodblogicalid", {
+      readCapacity: 3,
+      writeCapacity: 3,
+      partitionKey: { name: "customerid", type: dynamodb.AttributeType.NUMBER },
+      tableName: "demotablename",
     });
   }
 }
