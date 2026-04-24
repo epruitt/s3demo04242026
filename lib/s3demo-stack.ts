8adc7da23b6526347020f975204883ad42cd758d
epruitt <epruitt223@gmail.com>
2026-04-24 09:19:42 -0500

    add lambda function

--- lib/s3demo-stack.ts
+++ lib/s3demo-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class S3DemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -16,18 +17,26 @@ export class S3DemoStack extends cdk.Stack {
     // });
 
     // S3 Bucket
-    const s3demobucket = new s3.Bucket(this, "s3demobucket04242026", {
-      bucketName: "demos3bucket223",
+    const s3demobucket = new s3.Bucket(this, "s3DemoBucket04242026", {
+      bucketName: "demos3bucket212",
       versioned: true,
       publicReadAccess: false,
     });
 
     // dynamoDB table
-    const dynamoDBdemo = new dynamodb.Table(this, "dynamodblogicalid", {
+    const dynamoDBdemo = new dynamodb.Table(this, "dynamodbLogicalId", {
       readCapacity: 3,
       writeCapacity: 3,
-      partitionKey: { name: "customerid", type: dynamodb.AttributeType.NUMBER },
-      tableName: "demotablename",
+      partitionKey: { name: "customerId", type: dynamodb.AttributeType.NUMBER },
+      tableName: "demoTableName",
+    });
+
+    // lambda function
+    const demolambda = new lambda.Function(this, "logicalId", {
+      code: lambda.Code.fromAsset("../services/"),
+      handler: "lambda_function.lambda_handler",
+      runtime: lambda.Runtime.PYTHON_3_10,
+      functionName: "demoCdkLambda",
     });
   }
 }
